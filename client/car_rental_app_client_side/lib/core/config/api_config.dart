34b75b582fe3645a3727356ba677983b75760360
Thaleshwari <thaleshwari2005@gmail.com>
2026-08-08 17:24:50 +0530

    location_confirmation is fixed

--- client/car_rental_app_client_side/lib/core/config/api_config.dart
+++ client/car_rental_app_client_side/lib/core/config/api_config.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  static const String baseUrl = kDebugMode
-      ? 'http://localhost:5000'
-      : 'https://carrental-application-1.onrender.com';
+  ApiConfig._();
+
+  static const String baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'https://carrental-application-z49a.onrender.com',
+  );
 }
